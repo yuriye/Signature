4ddcf4a03fcf429e9ad71bac31752b8d94ceab96
Ю А. Елисеев <eliseev@MFC.net>
2017-08-31 17:47:20 +1200

    Initial commit

---
+++ app/src/main/java/com/yelisoft/signature/MainActivity.java
@@ -0,0 +1,42 @@
+package com.yelisoft.signature;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.actions_clear:
+                    mTextMessage.setText(R.string.title_clear);
+                    return true;
+                case R.id.actions_save:
+                    mTextMessage.setText(R.string.title_save);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.actions);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
